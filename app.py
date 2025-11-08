71122ad2ceb8eb3b1d29387a4072ca269432a063
chan <ctien2@buffalo.edu>
2025-11-08 18:55:16 -0500

    started checking location to parking lot

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html', message="Hello, Flask!")
+# @app.route('/')
+# def home():
+#     return render_template('index.html', message="Hello, Flask!")
 
 
+
+
+@app.route('/location')
+def check_location():
+    data = request.get_json()
+    user_latitude = data.get('user_latitude')
+    user_longitude = data.get('user_longitude')
+    lot_id = data.get('lot_id')
+    lot_latitude = 0.0 
+    lot_longitude = 0.0
+    if abs(lot_latitude - user_latitude) > 0.005 or abs(lot_longitude - user_longitude) > 0.005:
+        response.status_code = 404
+        return {}
 
 
 
