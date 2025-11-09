369d8f598eb84ba904ad9145f369bd6df5ed50da
chan <ctien2@buffalo.edu>
2025-11-08 20:13:11 -0500

    supabase setup

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = Flask(__name__)
+
+
+url = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
+key = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
+supabase: Client = create_client(url, key)
+
 
 # @app.route('/')
 # def home():
@@ -15,10 +25,13 @@ def check_location():
     user_latitude = data.get('user_latitude')
     user_longitude = data.get('user_longitude')
     lot_id = data.get('lot_id')
+#     const { data, error } = await supabase
+#   .from('characters')
+#   .select()
     lot_latitude = 0.0 
     lot_longitude = 0.0
     if abs(lot_latitude - user_latitude) > 0.005 or abs(lot_longitude - user_longitude) > 0.005:
-        response.status_code = 404
+        Response.status_code = 404
         return {}
 
 
