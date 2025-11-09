ceb5404e0c94fbeb05a2022d5bd087ae0e716494
Alvin <alvintsa@buffalo.edu>
2025-11-09 09:37:08 -0500

    yah

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  /* your other config options here */
 };
 
 export default nextConfig;
