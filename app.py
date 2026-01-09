829674d34c6045bddeb94f4a1f9c9cbd182e2c1d
Alvin <alvintsa@buffalo.edu>
2026-01-09 14:33:21 -0500

    Fixed cron route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response, jsonify, Blueprint
+from flask import Flask, request, Response, jsonify, Blueprint, make_response
 from flask_cors import CORS
 from supabase import create_client, Client
 from dotenv import load_dotenv
@@ -466,7 +466,7 @@ def get_live_cv_data():
 @api.route('/health', methods=['GET'])
 def health_check():
     """Health check endpoint for cron jobs to keep service alive."""
-    return {"", 204}
+    return make_response("", 204)
 
 @api.route('/ping', methods=['GET'])
 def ping():
